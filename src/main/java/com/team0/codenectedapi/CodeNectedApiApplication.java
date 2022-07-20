e371afa48dd3ac4ece8ba2853bf9a16df34809e3
Boyce007 <diboyce21@students.desu.edu>
2022-07-20 14:46:08 -0400

    Updates

---
+++ src/main/java/com/team0/codenectedapi/CodeNectedApiApplication.java
@@ -0,0 +1,13 @@
+package com.team0.codenectedapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeNectedApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeNectedApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team0/codenectedapi/CodeNectedApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team0.codenectedapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeNectedApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
